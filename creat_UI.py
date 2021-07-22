0d3cc579de10a96834e3d2539f3b89bcc3ba801e
Diwen <lidiwengz@gmail.com>
2021-07-22 01:14:30 -0600

    using menu list to create UI

--- creat_UI.py
+++ creat_UI.py
@@ -27,10 +27,32 @@ class Creat_UI(object):
         self.tab_list = {}
         self.toolbox_list = {}
 
-    def creat_tabs(self,menu_list) -> None:
+    def create_tabs(self, menu_list) -> None:
         for menu in menu_list:
             tab = QtWidgets.QWidget()
             name = "tab_%s" % menu
             tab.setObjectName(name)
             self.tab_list[name] = tab
+
+        return
+
+    def create_toolboxes(self, menu_list, tab_name) -> None:
+        if len(menu_list) == 0:
+            return
+
+        try:
+            current_tab = self.tab_list[tab_name]
+        except Exception as e:
+            print(e)
+            return
+
+        for menu in menu_list:
+            toolbox = QtWidgets.QToolBox(current_tab)
+            toolbox.setGeometry(QtCore.QRect(0, 10, 1000, 900))
+            toolbox.setFont(self.font_list[20])
+            toolbox.setObjectName(menu)
+
+        return
+
+    def create_button(self, btn_name_list) -> None:
         return
